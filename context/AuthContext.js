65d886814bc7520f8ba5357929eec000aec0fe35
Shin-sibainu <shincode0712@gmail.com>
2022-04-30 18:30:54 +0900

    mod

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { Magic } from "magic-sdk";
 import { MAGIC_PUBLIC_KEY } from "../utils/urls";
 import { useEffect } from "react";
+import axios from "axios";
 
 const AuthContext = createContext();
 
@@ -15,13 +16,33 @@ export const AuthProvider = (props) => {
 
   useEffect(() => {
     magic = new Magic(MAGIC_PUBLIC_KEY);
+    // const test = async () => {};
+    // test();
+    // console.log(user);
     checkUserLoggedIn();
   }, []);
 
   const login = async (email) => {
     try {
-      await magic.auth.loginWithMagicLink({ email });
-      setUser({ email });
+      // console.log(email);
+      //ユーザー登録
+      await axios
+        .post("http://localhost:1337/api/auth/local/register", {
+          username: email,
+          email: email,
+          password: email,
+        })
+        .then((res) => {
+          console.log("登録完了！");
+          console.log("User profile", res.data.user);
+          console.log("User token", res.data.jwt);
+          magic.auth.loginWithMagicLink({ email });
+          setUser({ email });
+        })
+        .catch((error) => {
+          // Handle error.
+          console.log("An error occurred:", error.response);
+        });
       router.push("/");
     } catch (err) {
       setUser(null);
@@ -44,9 +65,18 @@ export const AuthProvider = (props) => {
       if (isLoggedIn) {
         const { email } = await magic.user.getMetadata();
         setUser({ email });
+        // const token = await getToken();
+        // console.log("user token:" + token);
       }
     } catch (err) {}
   };
+
+  // const getToken = async () => {
+  //   try {
+  //     const token = await magic.user.getIdToken();
+  //     return token;
+  //   } catch (err) {}
+  // };
 
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
